21a2ab516fe006f37f0c53a3f0d6b311540cc0fd
Filip Stogowski <filip.stogowski@gmail.com>
2021-11-03 19:24:40 +0100

    Add human

--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -9,9 +9,11 @@ namespace Pizza
         {
             IAnimal dog = new Dog { Name = "Azor"};
             IAnimal cat = new Cat { Name = "Uwu" };
+            IAnimal human = new Human { Name = "Wojtek" };
 
             Console.WriteLine($"{dog.Name} says: {dog.GetSound()}");
             Console.WriteLine($"{cat.Name} says: {cat.GetSound()}");
+            Console.WriteLine($"{human.Name} says: {human.GetSound()}");
         }
     }
 }
